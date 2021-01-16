d215af8b2807ef0746b422ab2980e0c8a70930ce
Saul Corona <saulcoronasantos@ymail.com>
2021-01-16 14:41:19 -0600

    Add item search endpoint & ParseData util to get item object info

--- server/routes/items.js
+++ server/routes/items.js
@@ -1,10 +1,38 @@
-const express = require("express")
+const express = require("express");
 const router = express.Router();
 const axios = require("axios");
+const parseData = require("../utils/ParseData")
 
-//@route GET /api/items?q=​:query
-//@access Public
-router.get('/',(req,res) => res.send("User route"));
+const API_URL = process.env.API;
+
+
+//@route    GET /api/items?q=​:query
+//@desc     Get items from API
+//@access   Public
+router.get('/', async (req, res) => {
+    
+    const queryItem = req.query["q"];
+
+    if (queryItem !== undefined && queryItem !== "") {
+      try {
+        const responseItem = await axios.get(API_URL + "/sites/MLA/search", {
+          params: { q: encodeURI(queryItem) },
+        });
+  
+        parseData(res, responseItem.data, "search");
+      } catch (error) {
+        console.error(error);
+        parseData(res, { API: "Item not found" });
+      }
+    } else return parseData(res, { API: "Query Item not found" });
+  });
+  
+
+//@route    GET /api/items:id
+//@desc     Get single item from API
+//@access   Public
+
+
 
 
 
---
+++ server/utils/ParseData.js
@@ -0,0 +1,76 @@
+const parseData = (res, data = {}, type = "") => {
+    let response = {
+      author: {
+        name: process.env.AUTHOR_NAME,
+        lastname: process.env.LAST_NAME
+      },
+    };
+    const categories = [];
+  
+    switch (type) {
+
+      case "search":
+        
+        if (data.filters.length) {
+          const { path_from_root: pathFromRoot } = data.filters[0].values[0];
+          pathFromRoot.map((category) => categories.push(category.name));
+        }
+  
+        let items = [];
+        data.results
+          .slice(0, process.env.RETURN_ITEMS_NUMBER || 4)
+          .forEach((item, index) => {
+            const price = item.price.toString().split(".");
+            const decimals = price.length == 2 ? price[1] : "0";
+            items[index] = {
+              id: item.id,
+              title: item.title,
+              price: {
+                currency: item.currency_id,
+                amount: Number(price[0]),
+                decimals: Number(decimals),
+              },
+              picture: item.thumbnail,
+              condition: item.condition,
+              free_shipping: item.shipping.free_shipping,
+              location: item.address.state_name,
+            };
+          });
+        response = { ...response, categories, items };
+        break;
+  
+     
+      case "single":
+        
+        if (data.categories.path_from_root.length) {
+          const { path_from_root: pathFromRoot } = data.categories;
+          pathFromRoot.map((category) => categories.push(category.name));
+        }
+  
+        const price = data.item.price.toString().split(".");
+        const decimalSingle = data.item.price.length == 2 ? price[1] : "0";
+        const item = {
+          id: data.item.id,
+          title: data.item.title,
+          price: {
+            currency: data.item.currency_id,
+            amount: Number(price[0]),
+            decimals: Number(decimalSingle),
+          },
+          picture: data.item.pictures.length ? data.item.pictures[0].url : "",
+          condition: data.item.condition,
+          free_shipping: data.item.shipping.free_shipping,
+          sold_quantity: Number(data.item.sold_quantity),
+          description: data.description.plain_text,
+        };
+        response = { ...response, categories, item };
+        break;
+      default:
+        response = { ...response, data };
+        break;
+    }
+  
+    res.json(response);
+  };
+  
+  module.exports = parseData;
